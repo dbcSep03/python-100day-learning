afeffafda7c13185f53fcca6bbe74673ea7d8bf0
dbcSep03 <1251252418@qq.com>
2021-12-07 22:17:07 +0800

    第一天

---
+++ 1 dat turtle函数.py
@@ -0,0 +1,12 @@
+import turtle
+
+turtle.pensize(4)#笔的大小
+turtle.pencolor('red')#
+turtle.forward(100)#先前走
+turtle.right(90)#向右转90度
+turtle.forward(100)
+turtle.right(90)
+turtle.forward(100)
+turtle.right(90)
+turtle.forward(100)
+turtle.mainloop()
